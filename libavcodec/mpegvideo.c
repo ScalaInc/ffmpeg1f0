mpc-hc/FFmpeg

mpegvideo: null out current_picture_ptr when allocation failed
This avoids using an uninitialized picture in further decode calls.

libavcodec/mpegvideo.c
@@ -1359,8 +1359,10 @@ int ff_MPV_frame_start(MpegEncContext *s, AVCodecContext *avctx)
 
         pic->f.coded_picture_number = s->coded_picture_number++;
 
-        if (ff_alloc_picture(s, pic, 0) < 0)
+        if (ff_alloc_picture(s, pic, 0) < 0) {
+            s->current_picture_ptr = NULL;
             return -1;
+        }
 
         s->current_picture_ptr = pic;
         // FIXME use only the vars from current_pic

ScalaInc/ffmpeg1f0

libavcodec/mpegvideo.c
@@ -1245,8 +1245,10 @@ int ff_mpv_frame_start(MpegEncContext *s, AVCodecContext *avctx)
 
     pic->f->coded_picture_number = s->coded_picture_number++;
 
-    if (alloc_picture(s, pic, 0) < 0)
+    if (alloc_picture(s, pic, 0) < 0) {
+        s->current_picture_ptr = NULL;
         return -1;
+    }
 
     s->current_picture_ptr = pic;
     // FIXME use only the vars from current_pic