mpc-hc/FFmpeg

HACK: Add a function to manually add streams to the MPEG-TS demuxer
This is used by the BDMV demuxer when some streams are not detected by auto-probing

libavformat/mpegts.c
@@ -1673,6 +1673,31 @@ static void sdt_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
     }
 }
 
+int avpriv_mpegts_add_stream(AVFormatContext *s, int pid, uint32_t stream_type, int program)
+{
+    MpegTSContext *ts = s->priv_data;
+    MpegTSFilter *tss = ts->pids[pid];
+    PESContext *pes = NULL;
+    AVStream *st = NULL;
+
+    if (tss)
+        return -1;
+
+    pes = add_pes_stream(ts, pid, -1);
+    if (!pes)
+        return -1;
+
+    st = avformat_new_stream(pes->stream, NULL);
+    st->id = pes->pid;
+
+    if (!st)
+        return -1;
+
+    mpegts_set_stream_info(st, pes, stream_type, AV_RL32("HDMV"));
+    if (program >= 0)
+        ff_program_add_stream_index(s, program, st->index);
+}
+
 /* handle one TS packet */
 static int handle_packet(MpegTSContext *ts, const uint8_t *packet)
 {

ScalaInc/ffmpeg1f0

libavformat/mpegts.c
@@ -2834,6 +2834,32 @@ static int64_t mpegts_get_dts(AVFormatContext *s, int stream_index,
     return AV_NOPTS_VALUE;
 }
 
+int avpriv_mpegts_add_stream(AVFormatContext *s, int pid, uint32_t stream_type, int program)
+{
+    MpegTSContext *ts = s->priv_data;
+    MpegTSFilter *tss = ts->pids[pid];
+    PESContext *pes = NULL;
+    AVStream *st = NULL;
+
+    if (tss)
+        return -1;
+
+    pes = add_pes_stream(ts, pid, -1);
+    if (!pes)
+        return -1;
+
+    st = avformat_new_stream(pes->stream, NULL);
+    st->id = pes->pid;
+
+    if (!st)
+        return -1;
+
+    mpegts_set_stream_info(st, pes, stream_type, AV_RL32("HDMV"));
+    if (program >= 0)
+        av_program_add_stream_index(s, program, st->index);
+    return 0;
+}
+
 /**************************************************************/
 /* parsing functions - called from other demuxers such as RTP */
 