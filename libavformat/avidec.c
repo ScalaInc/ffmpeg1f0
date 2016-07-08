mpc-hc/FFmpeg

avidec: force probing of subtitle streams

libavformat/avidec.c
@@ -558,7 +558,8 @@ static int avi_read_header(AVFormatContext *s)
                 codec_type = AVMEDIA_TYPE_AUDIO;
                 break;
             case MKTAG('t', 'x', 't', 's'):
-                codec_type = AVMEDIA_TYPE_SUBTITLE;
+                st->codec->codec_type = codec_type = AVMEDIA_TYPE_SUBTITLE;
+                st->request_probe= 1;
                 break;
             case MKTAG('d', 'a', 't', 's'):
                 codec_type = AVMEDIA_TYPE_DATA;

ScalaInc/ffmpeg1f0

libavformat/avidec.c
@@ -686,7 +686,8 @@ static int avi_read_header(AVFormatContext *s)
                 codec_type = AVMEDIA_TYPE_AUDIO;
                 break;
             case MKTAG('t', 'x', 't', 's'):
-                codec_type = AVMEDIA_TYPE_SUBTITLE;
+                st->codecpar->codec_type = codec_type = AVMEDIA_TYPE_SUBTITLE;
+                st->request_probe= 1;
                 break;
             case MKTAG('d', 'a', 't', 's'):
                 codec_type = AVMEDIA_TYPE_DATA;