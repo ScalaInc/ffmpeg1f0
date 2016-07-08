mpc-hc/FFmpeg

Add "safe" full parsing mode.
In this mode, the parser does completely process the frame, but timestamps
are not mangled in compute_pkt_fields.

libavcodec/avcodec.h
@@ -4069,6 +4069,7 @@ typedef struct AVCodecParserContext {
 #define PARSER_FLAG_ONCE                      0x0002
 /// Set if the parser has a valid file offset
 #define PARSER_FLAG_FETCHED_OFFSET            0x0004
+#define PARSER_FLAG_NO_TIMESTAMP_MANGLING     0x0008
 
     int64_t offset;      ///< byte offset from starting packet start
     int64_t cur_frame_end[AV_PARSER_PTS_NB];

ScalaInc/ffmpeg1f0

libavcodec/avcodec.h
@@ -4959,6 +4959,7 @@ typedef struct AVCodecParserContext {
 /// Set if the parser has a valid file offset
 #define PARSER_FLAG_FETCHED_OFFSET            0x0004
 #define PARSER_FLAG_USE_CODEC_TS              0x1000
+#define PARSER_FLAG_NO_TIMESTAMP_MANGLING     0x2000
 
     int64_t offset;      ///< byte offset from starting packet start
     int64_t cur_frame_end[AV_PARSER_PTS_NB];