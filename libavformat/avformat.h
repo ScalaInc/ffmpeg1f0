mpc-hc/FFmpeg

avformat: add a new flag to let the user indicate the file is on a slow network device

libavformat/avformat.h
@@ -844,6 +844,7 @@ typedef struct AVFormatContext {
 #define AVFMT_FLAG_PRIV_OPT    0x20000 ///< Enable use of private options by delaying codec open (this could be made default once all code is converted)
 #define AVFMT_FLAG_KEEP_SIDE_DATA 0x40000 ///< Dont merge side data but keep it seperate.
 
+#define AVFMT_FLAG_NETWORK        0x40000000 ///< Source is a network protocol, optimize for that
 #define AVFMT_FLAG_IGNPARSERSYNC  0x80000000 ///< Don't use the parser syncing code
 
     /**

ScalaInc/ffmpeg1f0

libavformat/avformat.h
@@ -1447,6 +1447,8 @@ typedef struct AVFormatContext {
 #define AVFMT_FLAG_KEEP_SIDE_DATA 0x40000 ///< Don't merge side data but keep it separate.
 #define AVFMT_FLAG_FAST_SEEK   0x80000 ///< Enable fast, but inaccurate seeks for some formats
 
+#define AVFMT_FLAG_NETWORK        0x80000000 ///< Source is a network protocol, optimize for that
+
     /**
      * Maximum size of the data read from input for determining
      * the input container format.