d8cb7575e7b8f735e3292c901dfa228507d73419
Scala Install Builder <eng.admin@scala.com>
2017-02-27 16:12:14 -0500

    ffmpeg1f0 cmakelists.txt now supports an ENABLE_MP4PT2 option so we can toggle MPEG4 part 2 support. Added complimentary option to config4scala.sh to toggle.

--- config4scala.sh
+++ config4scala.sh
@@ -1,32 +1,37 @@
 #!/bin/sh
 # l:\m4\vc14build\ff>..\..\buildkansas\msys64\usr\bin\bash.exe --login ..\..\ffmpeg1f0\bff4scala.sh x64 release
-arch=x86
-archdir=x86
-debug=true
-installincludes=false
-targetdir=Debug
+ arch=x86
+ archdir=x86
+ debug=true
+ installincludes=false
+ targetdir=Debug
+ mp4pt2=""
 
-if [ -z "$MMOSROOT" ]; then
-    echo "envvar MMOSROOT is not set, exiting..."
-    exit 1
-fi 
+ if [ -z "$MMOSROOT" ]; then
+	 echo "envvar MMOSROOT is not set, exiting..."
+	 exit 1
+ fi 
 
-# convert path in MMOSROOT from Windows style to something MSYS2 can handle
-export MMOSROOTMSYS2=$(cygpath ${MMOSROOT})
+ # convert path in MMOSROOT from Windows style to something MSYS2 can handle
+ export MMOSROOTMSYS2=$(cygpath ${MMOSROOT})
 
-for opt in "$@"
-do
-    case "$opt" in
-    x86)
-            ;;
-    x64 | amd64)
-            arch=x86_64
-            archdir=x64
-            ;;
-    release)
-            debug=false
-			targetdir=RWDI
-            ;;
+ for opt in "$@"
+ do
+	 case "$opt" in
+	 x86)
+			 ;;
+	 x64 | amd64)
+			 arch=x86_64
+			 archdir=x64
+			 ;;
+	 release)
+			 debug=false
+			 targetdir=RWDI
+			 ;;
+	 mp4pt2)
+            echo "Got mp4pt2 Option"
+			 mp4pt2="mpeg4,"
+			 ;;
     *)
             echo "Unknown Option $opt"
             exit 1
@@ -54,7 +59,7 @@ configure() (
     --disable-iconv                    \
     --enable-hwaccel=*_dxva2           \
     --enable-protocol=file   \
-    --enable-decoder=dvvideo,h261,mpeg4,h264,hevc,mjpeg,mjpegb,mpeg1video,mpeg2video,mpegvideo,prores_lgpl,vc1,wmv*,aac,aac_latm,ac3,eac3,mp1*,mp2*,mp3*,wma*,pcm_bluray,pcm_dvd,pcm_f*,pcm_s*,pcm_lxf,pcm_mulaw,pcm_alaw,pcm_u*,adpcm_g*,adpcm_ima*,adpcm_ms  \
+    --enable-decoder=dvvideo,h261,${mp4pt2}h264,hevc,mjpeg,mjpegb,mpeg1video,mpeg2video,mpegvideo,prores_lgpl,vc1,wmv*,aac,aac_latm,ac3,eac3,mp1*,mp2*,mp3*,wma*,pcm_bluray,pcm_dvd,pcm_f*,pcm_s*,pcm_lxf,pcm_mulaw,pcm_alaw,pcm_u*,adpcm_g*,adpcm_ima*,adpcm_ms  \
     --enable-parser=aac,aac_latm,ac3,dvbsub,dvdsub,h261,h264,hevc,mjpeg,mpeg4video,mpegaudio,mpegvideo,vc1 \
     --enable-demuxer=mp3,aac,ac3,eac3,asf,avi,dv,g722,h261,h264,hevc,m4v,mjpeg,mov,mpegps,mpegts,mpegtsraw,mpegvideo,pcm_s*,pcm_u*,pcm_f*,pcm_mulaw,pcm_alaw,vc1*,wav,xwma"
 
@@ -62,17 +67,8 @@ configure() (
   ## I need zlib and others to be statically linked.  This used to be more elaborate, but I changed to -static and deleted all libz.dll.a and pthread.dll.a from my msys2 install.  Yuck.
   EXTRA_LDFLAGS="-static"
 
-  if "$arch"="x86" ; then
-	echo ${arch} is not x86_64
+  if test "$arch"="x86" ; then
     OPTIONS="--cpu=i686 ${OPTIONS}"
-  fi
-
-  if $debug ; then
-    EXTRA_CFLAGS="${EXTRA_CFLAGS}"
-    EXTRA_LDFLAGS="${EXTRA_LDFLAGS}"
-  else
-    EXTRA_CFLAGS="${EXTRA_CFLAGS}"
-    EXTRA_LDFLAGS="${EXTRA_LDFLAGS}"
   fi
 
   sh ${MMOSROOTMSYS2}/src/ffmpeg1f0/configure --extra-cflags="${EXTRA_CFLAGS}" --extra-ldflags="${EXTRA_LDFLAGS}" ${OPTIONS}
